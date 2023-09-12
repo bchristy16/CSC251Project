1e61a5afbf0e2bf0849aaec99dff3c16ce0d3ba7
bchristy16 <bchris16@email.cpcc.edu>
2023-09-11 22:21:13 -0400

    Added code for demo class

--- Policy.java
+++ Policy.java
@@ -1,1 +1,148 @@
 public class Policy
+{
+   private int policyNumber;
+   private int age;
+   
+   private double height;  //in inches
+   private double weight;  //in pounds
+   
+   private String providerName;
+   private String firstName;
+   private String lastName;
+   private String smokingStatus; //smoker or non-smoker
+   
+   public Policy()   // Initialize with default values
+   {
+      policyNumber = -1;
+      age = -1;
+      
+      height = -1;
+      weight = -1;
+      
+      providerName = "unknown";
+      firstName = "unknown";
+      lastName = "unknown";
+      smokingStatus = "unknown";
+   }
+   
+   // Initialize with given values
+   public Policy(int policyNumber, String providerName, String firstName, String lastName, int age, String smokingStatus, double height, double weight)
+   {
+      this.policyNumber = policyNumber;
+      this.age = age;
+      
+      this.height = height;
+      this.weight = weight;
+      
+      this.providerName = providerName;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.smokingStatus = smokingStatus;
+      
+   }
+   
+   public void setPolicyNumber(int policyNumber)   // Set policy number
+   {
+      this.policyNumber = policyNumber;
+   }
+   
+   public int getPolicyNumber()  // Get policy number
+   {
+      return policyNumber;
+   }
+   
+   public void setAge(int age)   // Set age
+   {
+      this.age = age;
+   }
+   
+   public int getAge()  // Get age
+   {
+      return age;
+   }
+   
+   public void setHeight(double height)   // Set height
+   {
+      this.height = height;
+   }
+   
+   public double getHeight()  // Get height
+   {
+      return height;
+   }
+   
+   public void setWeight(double weight)   // Set weight
+   {
+      this.weight = weight;
+   }
+   
+   public double getWeight()  // Get weight
+   {
+      return weight;
+   }
+   
+   public void setProviderName(String providerName) // Set provider name
+   {
+      this.providerName = providerName;
+   }
+   
+   public String getProviderName()  // Get provider name
+   {
+      return providerName;
+   }
+   
+   public void setFirstName(String firstName)   // Set first name
+   {
+      this.firstName = firstName;
+   }
+   
+   public String getFirstName()  // Get first name
+   {
+      return firstName;
+   }
+   
+   public void setLastName(String lastName)   // Set last name
+   {
+      this.lastName = lastName;
+   }
+   
+   public String getLastName()  // Get last name
+   {
+      return lastName;
+   }
+   
+   public void setSmokingStatus(String smokingStatus)   // Set smoking status
+   {
+      this.smokingStatus = smokingStatus;
+   }
+   
+   public String getSmokingStatus()  // Get smoking status
+   {
+      return smokingStatus;
+   }
+   
+   public double getBMI()  // Calculates the policy holder's BMI
+   {
+      return (weight*703) / (height*height);
+   }
+   
+   public double getPrice()   // Calculates the policy holder's fees
+   {
+      double baseFee = 600;
+      
+      if(age > 50)
+      {
+         baseFee = baseFee + 75;
+      }
+      if(smokingStatus == "smoker")
+      {
+         baseFee = baseFee + 100;
+      }
+      if(getBMI() > 35)
+      {
+         baseFee = baseFee + ((getBMI() - 35) * 20);
+      }
+      
+      return baseFee;
+   }
+}
--- Project_Benjamin_Christy.java
+++ Project_Benjamin_Christy.java
@@ -1,7 +1,72 @@
+import java.util.Scanner;
+
 public class Project_Benjamin_Christy
 {
    public static void main(String[] args)
    {
-   
+      Scanner scnr = new Scanner(System.in);
+      
+      Policy userPolicy;
+      
+      int policyNumber;
+      int age;
+      
+      double weight;
+      double height;
+      
+      String providerName;
+      String firstName;
+      String lastName;
+      String smokingStatus;
+      
+      System.out.print("Please enter the Policy Number: ");
+      policyNumber = scnr.nextInt();
+      scnr.nextLine();
+      
+      System.out.print("Please enter the Provider Name: ");
+      providerName = scnr.nextLine();
+      
+      System.out.print("Please enter the Policyholder's First Name: ");
+      firstName = scnr.nextLine();
+      
+      System.out.print("Please enter the Policyholder's Last Name: ");
+      lastName = scnr.nextLine();
+      
+      System.out.print("Please enter the Policyholder's Age: ");
+      age = scnr.nextInt();
+      scnr.nextLine();
+      
+      System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
+      smokingStatus = scnr.nextLine();
+      
+      System.out.print("Please enter the Policyholder's Height (in inches): ");
+      height = scnr.nextDouble();
+      scnr.nextLine();
+      
+      System.out.print("Please enter the Policyholder's Weight (in pounds: ");
+      weight = scnr.nextDouble();
+      scnr.nextLine();
+      
+      userPolicy = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
+      
+      System.out.println("/nPolicy Number: " + userPolicy.getPolicyNumber());
+      
+      System.out.println("Provider Name: " + userPolicy.getProviderName());
+      
+      System.out.println("Policyholder's First Name: " + userPolicy.getFirstName());
+      
+      System.out.println("Policyholder's Last Name: " + userPolicy.getLastName());
+      
+      System.out.println("Policyholder's age: " + userPolicy.getAge());
+      
+      System.out.println("Policyholder's Smoking Status: " + userPolicy.getSmokingStatus());
+      
+      System.out.println("Policyholder's Height: " + userPolicy.getHeight());
+      
+      System.out.println("Policyholder's Weight: " + userPolicy.getWeight());
+      
+      System.out.println("Policyholder's BMI: " + String.format("%.2f",userPolicy.getBMI()));
+      
+      System.out.println("Policy Price: $" + String.format("%.2f",userPolicy.getPrice()));
    }
 }
